cfe8db0ab3e4e04c00e1339142003b5369100376
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-01-13 11:11:24 +0000

    DL-7067 dependency and plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,13 +17,13 @@ import play.core.PlayVersion.current
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "5.16.0"
+  val bootstrapVersion = "5.19.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "simple-reactivemongo"      % "8.0.0-play-28",
     "uk.gov.hmrc"       %% "mongo-lock"                % "7.0.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "org.typelevel"     %% "cats-core"                 % "2.6.1",
+    "org.typelevel"     %% "cats-core"                 % "2.7.0",
     "com.chuusai"       %% "shapeless"                 % "2.3.7",
     "org.reactivemongo" %% "reactivemongo-akkastream"  % "0.20.13"
   )
@@ -33,24 +33,24 @@ object AppDependencies {
     "com.typesafe.play"            %% "play-test"              % current          % "test",
     "org.pegdown"                  %  "pegdown"                % "1.6.0"          % "test, it",
     "org.scalatestplus.play"       %% "scalatestplus-play"     % "5.1.0"          % "test, it",
-    "org.scalamock"                %% "scalamock"              % "5.1.0"          % "test, it",
+    "org.scalamock"                %% "scalamock"              % "5.2.0"          % "test, it",
     "org.scalatestplus"            %% "scalacheck-1-15"        % "3.2.10.0"       % "test, it",
-    "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.31.0"         % "test, it",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.12.5"         % "test, it",
+    "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.32.0"         % "test, it",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.1"         % "test, it",
     "com.miguno.akka"              %% "akka-mock-scheduler"    % "0.5.5"          % "test, it",
-    "com.typesafe.akka"            %% "akka-testkit"           % "2.6.17"         % "test, it"
+    "com.typesafe.akka"            %% "akka-testkit"           % "2.6.18"         % "test, it"
   )
 
   // Fixes a transitive dependency clash after upgrading to sbt 1.3.4
   val overrides: Seq[ModuleID] = {
     val jettyFromWiremockVersion = "9.4.44.v20210927"
     Seq(
-      "com.typesafe.akka"           %% "akka-actor"                 % "2.6.17",
-      "com.typesafe.akka"           %% "akka-stream"                % "2.6.17",
-      "com.typesafe.akka"           %% "akka-protobuf"              % "2.6.17",
-      "com.typesafe.akka"           %% "akka-slf4j"                 % "2.6.17",
-      "com.typesafe.akka"           %% "akka-serialization-jackson" % "2.6.17",
-      "com.typesafe.akka"           %% "akka-actor-typed"           % "2.6.17",
+      "com.typesafe.akka"           %% "akka-actor"                 % "2.6.18",
+      "com.typesafe.akka"           %% "akka-stream"                % "2.6.18",
+      "com.typesafe.akka"           %% "akka-protobuf"              % "2.6.18",
+      "com.typesafe.akka"           %% "akka-slf4j"                 % "2.6.18",
+      "com.typesafe.akka"           %% "akka-serialization-jackson" % "2.6.18",
+      "com.typesafe.akka"           %% "akka-actor-typed"           % "2.6.18",
       "org.eclipse.jetty"           % "jetty-client"       % jettyFromWiremockVersion,
       "org.eclipse.jetty"           % "jetty-continuation" % jettyFromWiremockVersion,
       "org.eclipse.jetty"           % "jetty-http"         % jettyFromWiremockVersion,
